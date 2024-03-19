e0ee19af55597e09bc86c2905c53562c7b33cf62
lekhya446 <110805868+lekhya446@users.noreply.github.com>
2024-03-19 21:44:20 +0530

    Add files via upload

---
+++ Sq.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Sq
+{
+public static void main(String[] args)
+{
+int n;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the number");
+n=sc.nextInt();
+int k=1;
+for(int i=1;i<=n;i++)
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print(k*k+" ");
+k++;
+}
+System.out.println();
+}
+}
+}
