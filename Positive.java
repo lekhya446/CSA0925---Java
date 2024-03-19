27c1e7f3f3c5d8e135c7f12ca90e0e92dd113da2
lekhya446 <110805868+lekhya446@users.noreply.github.com>
2024-03-19 21:41:27 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Positive
+{
+public static void main(String args[])
+{
+ int n=30;
+if(n>0)
+{
+System.out.println("positive");
+}
+else
+{
+System.out.println("Negative");
+}
+}
+}
